e45f92a8edab7d96c38a69163c1780c78521b557
Tyler Cox <tmcox227@learner.dvusd.org>
2024-01-05 09:30:32 -0700

    hundred

--- Converter/ConsoleApp9/Program.cs
+++ Converter/ConsoleApp9/Program.cs
@@ -10,6 +10,12 @@ namespace ConsoleApp9
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+                
+            }
+            Console.ReadKey();
         }
     }
 }
